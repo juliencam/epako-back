4767e0c07870d6f31a4da0297b173aeb615b4519
julien-cambien <juliencambien@gmail.com>
2021-03-25 14:11:08 +0100

    julien install easyadmin add crud controller for all entity

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Place;
+use App\Entity\Review;
+use App\Entity\Product;
+use App\Entity\Department;
+use App\Entity\PlaceCategory;
+use App\Entity\ProductCategory;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Admin epako');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Products', 'fas fa-tags', Product::class);
+        yield MenuItem::linkToCrud('Category Product', 'fas fa-tags', ProductCategory::class);
+        yield MenuItem::linkToCrud('Place', 'fas fa-tags', Place::class);
+        yield MenuItem::linkToCrud('Place Category', 'fas fa-tags', PlaceCategory::class);
+        yield MenuItem::linkToCrud('Department', 'fas fa-tags', Department::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-tags', User::class);
+        yield MenuItem::linkToCrud('Review', 'fas fa-tags', Review::class);
+    }
+}
