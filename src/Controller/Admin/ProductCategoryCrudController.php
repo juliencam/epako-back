655d36455c050cb6f5775a3243a93c7caa925c16
julien-cambien <juliencambien@gmail.com>
2021-03-25 14:53:49 +0100

    julien update fixture picto

--- src/Controller/Admin/ProductCategoryCrudController.php
+++ src/Controller/Admin/ProductCategoryCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\ProductCategory;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProductCategoryCrudController extends AbstractCrudController
@@ -12,14 +15,15 @@ class ProductCategoryCrudController extends AbstractCrudController
         return ProductCategory::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
+        
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextEditorField::new('picto'),
         ];
     }
-    */
+
 }
