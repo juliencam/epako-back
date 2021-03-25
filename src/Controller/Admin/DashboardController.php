939a554453a85a872beb57244c5f5e98a4ec1610
julien-cambien <juliencambien@gmail.com>
2021-03-25 14:33:31 +0100

    julien install easyadmin add crud controller for all entity

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -34,12 +34,15 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Product');
         yield MenuItem::linkToCrud('Products', 'fas fa-tags', Product::class);
         yield MenuItem::linkToCrud('Category Product', 'fas fa-tags', ProductCategory::class);
+        yield MenuItem::section('Place');
         yield MenuItem::linkToCrud('Place', 'fas fa-tags', Place::class);
         yield MenuItem::linkToCrud('Place Category', 'fas fa-tags', PlaceCategory::class);
         yield MenuItem::linkToCrud('Department', 'fas fa-tags', Department::class);
-        yield MenuItem::linkToCrud('User', 'fas fa-tags', User::class);
         yield MenuItem::linkToCrud('Review', 'fas fa-tags', Review::class);
+        yield MenuItem::section('User');
+        yield MenuItem::linkToCrud('User', 'fas fa-tags', User::class);
     }
 }
