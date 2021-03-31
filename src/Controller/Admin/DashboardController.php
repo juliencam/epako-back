18eb0aba0271657dbad47cb48b082b2a47f2ee35
Frederic Lescure <magione1004@gmail.com>
2021-03-31 11:07:08 +0200

    fred/ add second annotation @Routes on place Controller

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -33,7 +33,7 @@ class DashboardController extends AbstractDashboardController
         return Dashboard::new()
             ->setTitle('Admin epako');
     }
-    
+
 
     public function configureMenuItems():iterable
     {
