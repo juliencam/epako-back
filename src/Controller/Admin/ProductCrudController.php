839b199cb41ecf94b3611d3edbe7dd1add915e65
Frederic Lescure <magione1004@gmail.com>
2021-04-11 14:28:29 +0200

    test push all branch

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -84,7 +84,7 @@ class ProductCrudController extends AbstractCrudController
         // ->andWhere('pc.id = :val')
         // ->setParameter('val', $value)
         // ->getQuery()
-        // ->getResult()ff
+        // ->getResult()
 
         return [
             IntegerField::new('id')->onlyOnIndex(),
