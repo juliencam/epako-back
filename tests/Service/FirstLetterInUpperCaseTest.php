c2732a7bddbaade21c132f36d642c9b0e6e42bec
julien-cambien <juliencambien@gmail.com>
2021-04-16 14:18:58 +0200

    julien/ create unit testing on FirstLetterInUpperCase

---
+++ tests/Service/FirstLetterInUpperCaseTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\FirstLetterInUpperCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * Si on on veut où que l'on doit(ve) passer par le conteneur de services
+ */
+class FirstLetterInUpperCaseTest extends KernelTestCase
+{
+    public function testChangeFirstLetterInUpperCase(): void
+    {
+        // On veut accéder au conteneur de services depuis le Kernel
+        // (plutôt que de passer notre service en public dans services.yaml)
+        self::bootKernel();
+        $container = self::$container;
+        // On récupère notre service depuis le conteneur
+        $firstLetterInUpperCase = $container->get(FirstLetterInUpperCase::class);
+
+        $firstLetterInUpperCase->setFirstLetterInUpperCase(true);
+
+
+        $wordTest = $firstLetterInUpperCase->changeFirstLetter('test');
+
+        // Vérifier qu'elle est correcte
+        $this->assertEquals('Test', $wordTest);
+    }
+
+    public function testNotToChangeFirstLetterInUpperCase(): void
+    {
+        // On veut accéder au conteneur de services depuis le Kernel
+        // (plutôt que de passer notre service en public dans services.yaml)
+        self::bootKernel();
+        $container = self::$container;
+        // On récupère notre service depuis le conteneur
+        $firstLetterInUpperCase = $container->get(FirstLetterInUpperCase::class);
+
+        $firstLetterInUpperCase->setFirstLetterInUpperCase(false);
+
+
+        $wordTest = $firstLetterInUpperCase->changeFirstLetter('test');
+
+        // Vérifier qu'elle est correcte
+        $this->assertEquals('test', $wordTest);
+    }
+
+}
